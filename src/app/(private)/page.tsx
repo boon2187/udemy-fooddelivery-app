7ad7c46a80a2ec18e032f0a66e29a6b19517c9ac
boon2187 <fumiya.mai.ito@gmail.com>
2025-10-23 21:19:10 +0900

    共通するレイアウトの部品としてSectionを作成し、Homeページに反映

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,3 +1,11 @@
+import Section from "@/components/section";
+
 export default function Home() {
-  return <div className="min-h-screen">Home</div>;
+  return (
+    <div className="min-h-screen">
+      <Section title="近くのお店">
+        <div>scroll_area</div>
+      </Section>
+    </div>
+  );
 }
