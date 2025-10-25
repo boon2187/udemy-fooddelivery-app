3eb2a12447d785fd2844658e55c5bb69185df1fc
boon2187 <fumiya.mai.ito@gmail.com>
2025-10-25 09:22:21 +0900

    不要なdivタグを削除

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -2,10 +2,8 @@ import Section from "@/components/section";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      <Section title="近くのお店">
-        <div>scroll_area</div>
-      </Section>
-    </div>
+    <Section title="近くのお店">
+      <div>scroll_area</div>
+    </Section>
   );
 }
