6e3fba148e4bdc1ab200ff5aa440303d5518e0ee
boon2187 <fumiya.mai.ito@gmail.com>
2025-11-04 21:50:32 +0900

    近くのレストランも表示できるように追加

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,16 +1,32 @@
 import CarouselContainer from "@/components/carousel-container";
 import RestaurantCard from "@/components/restaurant-card";
 import Section from "@/components/section";
-import { getRamenRestaurants } from "@/lib/restaurants/api";
+import { getRamenRestaurants, getRestaurants } from "@/lib/restaurants/api";
 
 export default async function Home() {
-  const { data: nearbyRamenRestaurants, error } = await getRamenRestaurants();
+  const { data: nearbyRamenRestaurants, error: ramenError } =
+    await getRamenRestaurants();
+  const { data: nearbyRestaurants, error: restaurantError } =
+    await getRestaurants();
   return (
     <>
-      {!nearbyRamenRestaurants ? (
-        <p>{error}</p>
+      {!nearbyRestaurants ? (
+        <p>{restaurantError}</p>
       ) : nearbyRamenRestaurants.length > 0 ? (
-        <Section title="近くのお店">
+        <Section title="近くのレストラン">
+          <CarouselContainer slideNumber={4}>
+            {nearbyRestaurants.map((restaurant) => (
+              <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+            ))}
+          </CarouselContainer>
+        </Section>
+      ) : (
+        <p>近くのレストランが見つかりません</p>
+      )}
+      {!nearbyRamenRestaurants ? (
+        <p>{ramenError}</p>
+      ) : nearbyRamenRestaurants.length > 0 ? (
+        <Section title="近くのラーメン店">
           <CarouselContainer slideNumber={4}>
             {nearbyRamenRestaurants.map((restaurant) => (
               <RestaurantCard key={restaurant.id} restaurant={restaurant} />
--- src/lib/restaurants/api.ts
+++ src/lib/restaurants/api.ts
@@ -1,6 +1,88 @@
 import { GooglePlacesSearchApiResponse, Restaurant } from "@/types";
 import { transformPlaceResults } from "./utils";
 
+// 近くのレストランを取得
+export async function getRestaurants(): Promise<{
+  data: Restaurant[];
+  error?: string;
+}> {
+  const url = "https://places.googleapis.com/v1/places:searchNearby";
+
+  const apiKey = process.env.GOOGLE_API_KEY;
+  const header = {
+    "Content-Type": "application/json",
+    "X-Goog-Api-key": apiKey!,
+    "X-Goog-FieldMask":
+      "places.id,places.displayName,places.primaryType,places.photos",
+  };
+
+  const desiredTypes = [
+    "japanese_restaurant",
+    "cafe",
+    "cafeteria",
+    "coffee_shop",
+    "chinese_restaurant",
+    "fast_food_restaurant",
+    "hamburger_restaurant",
+    "french_restaurant",
+    "italian_restaurant",
+    "pizza_restaurant",
+    "ramen_restaurant",
+    "sushi_restaurant",
+    "korean_restaurant",
+    "indian_restaurant",
+  ];
+
+  const requestBody = {
+    includedTypes: desiredTypes,
+    maxResultCount: 10,
+    locationRestriction: {
+      circle: {
+        center: {
+          latitude: 36.2307643,
+          longitude: 137.9627271,
+        },
+        radius: 500.0,
+      },
+    },
+    languageCode: "ja",
+  };
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(requestBody),
+    headers: header,
+    next: { revalidate: 86400 }, // 24時間でキャッシュを更新
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    console.error(errorData);
+    return {
+      data: [],
+      error: `NearbySearchリクエスト失敗: ${response.status}`,
+    };
+  }
+
+  const data: GooglePlacesSearchApiResponse = await response.json();
+  console.log(data);
+
+  if (!data.places) {
+    return { data: [] };
+  }
+  const nearbyRestaurants = data.places;
+
+  const matchedRestaurants = nearbyRestaurants.filter(
+    (place) => place.primaryType && desiredTypes.includes(place.primaryType)
+  );
+  console.log("matchedRestaurants", matchedRestaurants);
+
+  const restaurants = await transformPlaceResults(matchedRestaurants);
+  console.log("restaurants", restaurants);
+  return { data: restaurants };
+}
+
+// 近くのラーメン店を取得
 export async function getRamenRestaurants(): Promise<{
   data: Restaurant[];
   error?: string;
