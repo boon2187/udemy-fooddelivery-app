620aba105888f336a8110aaf790c95e559fd0017
boon2187 <fumiya.mai.ito@gmail.com>
2025-11-04 22:09:27 +0900

    Sectionコンポーネント内で切り替えたいので、TextToggleButtonの内容を移動

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,20 +1,32 @@
-import { ReactNode } from "react";
+"use client";
+
+import { ReactNode, useState } from "react";
 import { Button } from "./ui/button";
-import TextToggleButton from "./text-toggle-button";
 
 interface SectionProps {
   children: ReactNode;
   title: string;
+  expandedContent?: ReactNode;
 }
 
-export default function Section({ children, title }: SectionProps) {
+export default function Section({
+  children,
+  title,
+  expandedContent,
+}: SectionProps) {
+  const [isExpanded, setIsExpandes] = useState(false);
+  const handleChange = () => {
+    setIsExpandes((prev) => !prev);
+  };
   return (
     <section>
       <div className="flex items-center justify-between py-4">
         <h2 className="text-2xl font-bold">{title}</h2>
-        <TextToggleButton />
+        <Button onClick={handleChange}>
+          {isExpanded ? "表示を戻す" : "すべて表示"}
+        </Button>
       </div>
-      {children}
+      {isExpanded ? expandedContent : children}
     </section>
   );
 }
