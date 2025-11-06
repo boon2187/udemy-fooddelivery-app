879d19486428e5a1b89545d9b94395cf4693589b
boon2187 <fumiya.mai.ito@gmail.com>
2025-11-07 07:25:57 +0900

    カテゴリーの雛形表示まで

--- src/components/restaurant-list.tsx
+++ src/components/restaurant-list.tsx
@@ -1,3 +1,16 @@
-export default function RestaurantList() {
-  return <div>restaurant-list</div>;
+import { Restaurant } from "@/types";
+import RestaurantCard from "./restaurant-card";
+
+interface RestaurantListProps {
+  restaurants: Restaurant[];
+}
+
+export default function RestaurantList({ restaurants }: RestaurantListProps) {
+  return (
+    <ul className="grid grid-cols-4 gap-4">
+      {restaurants.map((restaurant) => (
+        <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+      ))}
+    </ul>
+  );
 }
