78bc6763affe2a22c43043dfcba2e408561736b9
boon2187 <fumiya.mai.ito@gmail.com>
2025-11-10 08:12:42 +0900

    検索欄に入力があったとき、サジェスチョンが出るように修正

--- src/components/place-search-bar.tsx
+++ src/components/place-search-bar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Command,
   CommandEmpty,
@@ -5,19 +7,35 @@ import {
   CommandItem,
   CommandList,
 } from "@/components/ui/command";
+import { useState } from "react";
 
 export default function PlaceSearchBar() {
+  const [open, setOpen] = useState(false);
+  const [inputText, setInputText] = useState("");
+
   return (
     <Command className="overflow-visible bg-muted" shouldFilter={false}>
-      <CommandInput placeholder="Type a command or search..." className="" />
-      <div className="relative">
-        <CommandList className="absolute bg-background w-full">
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandItem>Calendar</CommandItem>
-          <CommandItem>Search Emoji</CommandItem>
-          <CommandItem>Calculator</CommandItem>
-        </CommandList>
-      </div>
+      <CommandInput
+        value={inputText}
+        placeholder="Type a command or search..."
+        className=""
+        onValueChange={(text) => {
+          if (!open) {
+            setOpen(true);
+          }
+          setInputText(text);
+        }}
+      />
+      {open && (
+        <div className="relative">
+          <CommandList className="absolute bg-background w-full">
+            <CommandEmpty>No results found.</CommandEmpty>
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandList>
+        </div>
+      )}
     </Command>
   );
 }
