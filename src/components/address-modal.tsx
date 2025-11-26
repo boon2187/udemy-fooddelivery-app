d4f7818ba07b4d423bf3d73adb3cea68bcd9046e
boon2187 <fumiya.mai.ito@gmail.com>
2025-11-26 22:08:17 +0900

    AddressModalコンポーネントにおいて、サジェスチョンの型を明示化し、ローディングおよびエラーメッセージの表示を改善。

--- src/components/address-modal.tsx
+++ src/components/address-modal.tsx
@@ -23,11 +23,13 @@ import { useEffect, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
 
 import { v4 as uuidv4 } from "uuid";
+import { AddressSuggestion } from "@/types";
+import { AlertCircle, LoaderCircle, MapPin } from "lucide-react";
 
 export default function AddressModal() {
   const [inputText, setInputText] = useState("");
   const [sessionToken, setSessionToken] = useState(uuidv4());
-  const [suggestions, setSuggestions] = useState([]);
+  const [suggestions, setSuggestions] = useState<AddressSuggestion[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
@@ -49,7 +51,7 @@ export default function AddressModal() {
         setErrorMessage(errorData.error);
         return;
       }
-      const data = await response.json();
+      const data: AddressSuggestion[] = await response.json();
       console.log("suggestions data", data);
       setSuggestions(data);
     } catch (error) {
@@ -92,10 +94,34 @@ export default function AddressModal() {
             {inputText ? (
               // サジェスチョンを表示
               <>
-                <CommandEmpty>No results found.</CommandEmpty>
-                <div>サジェスチョン表示</div>
+                <CommandEmpty>
+                  <div className="flex items-center justify-center">
+                    {isLoading ? (
+                      <LoaderCircle className="animate-spin" />
+                    ) : errorMessage ? (
+                      <div>
+                        <AlertCircle />
+                        {errorMessage}
+                      </div>
+                    ) : (
+                      "住所が見つかりません"
+                    )}
+                  </div>
+                </CommandEmpty>
+                {suggestions.map((suggestion) => (
+                  <CommandItem key={suggestion.placeId} className="p-5">
+                    <MapPin />
+                    <div>
+                      <p className="font-bold">{suggestion.placeName}</p>
+                      <p className="text-muted-foreground">
+                        {suggestion.address_text}
+                      </p>
+                    </div>
+                  </CommandItem>
+                ))}
               </>
             ) : (
+              // 保存済みの住所を表示
               <>
                 <h3 className="font-bold text-lg mb-2">保存済みの住所</h3>
                 <CommandItem className="p-5">Calendar</CommandItem>
