b8854f26ac5fe3906e722cafd8153d8070a9f0c5
boon2187 <fumiya.mai.ito@gmail.com>
2026-01-11 09:19:06 +0900

    カテゴリサイドバーコンポーネントに、カテゴリー名を表示するメニュー表示機能を追加

--- src/components/category-sidebar.tsx
+++ src/components/category-sidebar.tsx
@@ -9,5 +9,20 @@ export default function CategorySidebar({
   categoryMenus,
 }: CategorySidebarProps) {
   console.log("カテゴリーサイドバーのメニュー", categoryMenus);
-  return <aside className="w-1/4 bg-green-200">category-sidebar</aside>;
+  return (
+    <aside className="w-1/4 bg-green-200">
+      <p className="p-3 font-bold">メニュー Menu</p>
+      <nav>
+        <ul>
+          {categoryMenus.map((category) => (
+            <li key={category.id}>
+              <button className="bg-red-300 w-full p-4 text-left">
+                {category.categoryName}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
 }
