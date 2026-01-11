9b501470edfee13a2b5a70641e3fac4e4ffddd91
boon2187 <fumiya.mai.ito@gmail.com>
2026-01-11 09:03:03 +0900

    カテゴリサイドバーコンポーネントを追加し、サイドバーとメニューエリアを作成

---
+++ src/components/category-sidebar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CategorySidebar() {
+  return <aside className="w-1/4 bg-green-200">category-sidebar</aside>;
+}
--- src/components/menu-content.tsx
+++ src/components/menu-content.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import CategorySidebar from "./category-sidebar";
 
 export default function MenuContent() {
-  return <div>menu-content</div>;
+  return (
+    <div className="flex gap-4">
+      <CategorySidebar />
+      <div className="w-3/4 bg-blue-400">menu</div>
+    </div>
+  );
 }
