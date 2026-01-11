3769760a2c03c54d1f37f5687d910f78734a8541
boon2187 <fumiya.mai.ito@gmail.com>
2026-01-11 10:57:58 +0900

    メニューの骨組みとして、レストランページにカテゴリメニューを表示する機能を追加

--- src/components/menu-content.tsx
+++ src/components/menu-content.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import CategorySidebar from "./category-sidebar";
 import { CategoryMenu } from "@/types";
+import Section from "./section";
 
 interface MenuContentProps {
   categoryMenus: CategoryMenu[];
@@ -10,7 +11,17 @@ export default function MenuContent({ categoryMenus }: MenuContentProps) {
   return (
     <div className="flex gap-4">
       <CategorySidebar categoryMenus={categoryMenus} />
-      <div className="w-3/4 bg-blue-400">menu</div>
+      <div className="w-3/4 bg-blue-400">
+        {categoryMenus.map((category) => (
+          <Section key={category.id} title={category.categoryName}>
+            {category.id === "featured" ? (
+                <div>すくろーるこんてんつ</div>
+            ) : (
+                <div>りすとこんてんつ</div>
+            )}
+          </Section>
+        ))}
+      </div>
     </div>
   );
 }
