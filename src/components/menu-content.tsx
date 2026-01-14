0a415e23e4d6b774c456ad2f383d4e8bee43221e
boon2187 <fumiya.mai.ito@gmail.com>
2026-01-14 23:13:06 +0900

    メニューコンテンツのカテゴリ選択時にスムーズスクロール機能を追加

--- src/components/menu-content.tsx
+++ src/components/menu-content.tsx
@@ -15,6 +15,11 @@ interface MenuContentProps {
 export default function MenuContent({ categoryMenus }: MenuContentProps) {
   const handleSelectCategory = (categoryId: string) => {
     console.log("Selected category ID:", categoryId);
+    const element = document.getElementById(`${categoryId}-menu`);
+
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
   };
 
   return (
@@ -25,21 +30,27 @@ export default function MenuContent({ categoryMenus }: MenuContentProps) {
       />
       <div className="w-3/4 bg-blue-400">
         {categoryMenus.map((category) => (
-          <Section key={category.id} title={category.categoryName}>
-            {category.id === "featured" ? (
-              <CarouselContainer slideNumber={4}>
-                {category.items.map((menu) => (
-                  <MenuCard key={menu.id} menu={menu} />
-                ))}
-              </CarouselContainer>
-            ) : (
-              <div className="grid grid-cols-2 gap-4">
-                {category.items.map((menu) => (
-                  <FlatMenuCard key={menu.id} menu={menu} />
-                ))}
-              </div>
-            )}
-          </Section>
+          <div
+            key={category.id}
+            id={`${category.id}-menu`}
+            className="scroll-mt-16"
+          >
+            <Section title={category.categoryName}>
+              {category.id === "featured" ? (
+                <CarouselContainer slideNumber={4}>
+                  {category.items.map((menu) => (
+                    <MenuCard key={menu.id} menu={menu} />
+                  ))}
+                </CarouselContainer>
+              ) : (
+                <div className="grid grid-cols-2 gap-4">
+                  {category.items.map((menu) => (
+                    <FlatMenuCard key={menu.id} menu={menu} />
+                  ))}
+                </div>
+              )}
+            </Section>
+          </div>
         ))}
       </div>
     </div>
