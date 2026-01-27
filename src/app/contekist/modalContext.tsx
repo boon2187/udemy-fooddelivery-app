0d6397c20251dc23f314abdf97b0554bd1ca77b0
boon2187 <fumiya.mai.ito@gmail.com>
2026-01-27 22:03:39 +0900

    ModalContextを新規作成し、モーダルの状態管理機能を追加

---
+++ src/app/contekist/modalContext.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Menu } from "@/types";
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useState } from "react";
+
+interface ModalContextType {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  openModal: (menu: Menu) => void;
+  closeModal: () => void;
+  selectedItem: Menu | null;
+}
+const ModalContext = createContext<ModalContextType | undefined>(undefined);
+
+export const ModalProvider = ({ children }: { children: ReactNode }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState<Menu | null>(null);
+
+  const openModal = (menu: Menu) => {
+    setIsOpen(true);
+    setSelectedItem(menu);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setSelectedItem(null);
+  };
+
+  return (
+    <ModalContext.Provider value={{ isOpen, setIsOpen, openModal, closeModal, selectedItem }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModalはModalProvider内で使用する必要があります。");
+  }
+  return context;
+};
