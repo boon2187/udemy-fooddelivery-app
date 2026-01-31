6731a69c4d9b6d656021ebe65788da81286eea6a
boon2187 <fumiya.mai.ito@gmail.com>
2026-01-31 09:42:24 +0900

    ホームページのメニューをクリックしたら、そのレストランのページに飛び、クリックしたメニューのモーダルを開くように処理を追加

--- src/app/(private)/page.tsx
+++ src/app/(private)/page.tsx
@@ -1,12 +1,14 @@
 import CarouselContainer from "@/components/carousel-container";
 import Categories from "@/components/categories";
 import MenuCard from "@/components/menu-card";
+import MenuClient from "@/components/menu-client";
 import MenuList from "@/components/menu-list";
 import RestaurantCard from "@/components/restaurant-card";
 import RestaurantList from "@/components/restaurant-list";
 import Section from "@/components/section";
 import { getMenus } from "@/lib/menus/api";
 import { fetchLocation, getRamenRestaurants, getRestaurants } from "@/lib/restaurants/api";
+import Link from "next/link";
 
 export default async function Home() {
   const { lat, lng } = await fetchLocation();
@@ -65,13 +67,12 @@ export default async function Home() {
       {/* メニュー情報の表示 */}
       {!menus ? (
         <p>{menusError}</p>
-      ) : menus.length > 0 ? (
-        <Section title={restaurant?.restaurantName} expandedContent={<MenuList menus={menus} />}>
-          <CarouselContainer slideNumber={6}>
-            {menus.map((menu) => (
-              <MenuCard menu={menu} />
-            ))}
-          </CarouselContainer>
+      ) : menus.length > 0 && restaurant ? (
+        <Section
+          title={restaurant.restaurantName}
+          expandedContent={<MenuList menus={menus} restaurant={restaurant} />}
+        >
+          <MenuClient menus={menus} restaurant={restaurant} />
         </Section>
       ) : (
         <p>メニューが見つかりません</p>
--- src/components/menu-list.tsx
+++ src/components/menu-list.tsx
@@ -1,16 +1,24 @@
-import { Menu } from "@/types";
+"use client";
+
+import { Menu, Restaurant } from "@/types";
 import MenuCard from "./menu-card";
+import Link from "next/link";
+import { useModal } from "@/app/context/modalContext";
 
 interface MenuListProps {
   menus: Menu[];
+  restaurant: Restaurant;
 }
 
-export default function MenuList({ menus }: MenuListProps) {
+export default function MenuList({ menus, restaurant }: MenuListProps) {
+  const { openModal } = useModal();
   return (
     <>
       <ul className="grid grid-cols-6 gap-4">
         {menus.map((menu) => (
-          <MenuCard key={menu.id} menu={menu} />
+          <Link href={`/restaurant/${restaurant.id}`} key={menu.id}>
+            <MenuCard menu={menu} onClick={openModal} />
+          </Link>
         ))}
       </ul>
     </>
