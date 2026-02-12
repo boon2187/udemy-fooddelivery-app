ddd11f73acb951f3cf9728ca6b94e87da3bfe69b
boon2187 <fumiya.mai.ito@gmail.com>
2026-02-12 22:45:03 +0900

    カート情報を全県取得する

---
+++ src/app/api/cart/route.ts
@@ -0,0 +1,43 @@
+import { createClient } from "@/utils/supabase/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  try {
+    const supabase = await createClient();
+
+    const {
+      data: { user },
+      error: userError,
+    } = await supabase.auth.getUser();
+
+    if (userError || !user) {
+      return NextResponse.json({ error: "ユーザーが認証されていません。" }, { status: 401 });
+    }
+
+    const { data: carts, error: cartsError } = await supabase
+      .from("carts")
+      .select(
+        `
+        id, restaurant_id,
+        cart_items (
+        id, quantity,
+        menus (
+            id, name, price, image_path)
+        )
+    `,
+      )
+      .eq("user_id", user.id);
+
+    if (cartsError) {
+      console.error("カート情報の取得に失敗", cartsError);
+      return NextResponse.json({ error: "カート情報の取得に失敗" }, { status: 500 });
+    }
+
+    console.log("cartです。", carts);
+
+    return NextResponse.json(carts);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "予期せぬエラーが発生しました。" }, { status: 500 });
+  }
+}
--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { useCart } from "@/hooks/cart/useCart";
+
 export default function Cart() {
+  const { carts } = useCart();
+  console.log("cart component:", carts);
   return <div>cart</div>;
 }
