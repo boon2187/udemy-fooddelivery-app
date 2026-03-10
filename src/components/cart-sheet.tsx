1303c3c69992251c8019c5775d11b3a16746cde8
boon2187 <fumiya.mai.ito@gmail.com>
2026-03-11 07:05:38 +0900

    カートシートの店名をクリックしたらシートが閉じるように修正

--- src/components/cart-sheet.tsx
+++ src/components/cart-sheet.tsx
@@ -46,9 +46,11 @@ export default function CartSheet({ cart, count, isOpen, openCart, closeCart }: 
         {cart ? (
           <>
             <div className="flex justify-between items-center">
-              <Link className="font-bold text-2xl" href={`/restaurant/${cart.restaurant_id}`}>
-                {cart.restaurantName}
-              </Link>
+              <SheetClose asChild>
+                <Link className="font-bold text-2xl" href={`/restaurant/${cart.restaurant_id}`}>
+                  {cart.restaurantName}
+                </Link>
+              </SheetClose>
               <TooltipProvider>
                 <Tooltip>
                   <TooltipTrigger asChild>
