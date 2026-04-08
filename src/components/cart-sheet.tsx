16b93a1b9550888780f91528670fddce8b8a446d
boon2187 <fumiya.mai.ito@gmail.com>
2026-04-08 22:15:42 +0900

    注文ページの雛形を作成

--- src/components/cart-sheet.tsx
+++ src/components/cart-sheet.tsx
@@ -162,7 +162,7 @@ export default function CartSheet({
             </div>
             <SheetClose asChild>
               <Button asChild>
-                <Link href={`/checkout/${cart.restaurant_id}`}>お会計に進む</Link>
+                <Link href={`/restaurant/${cart.restaurant_id}/checkout`}>お会計に進む</Link>
               </Button>
             </SheetClose>
           </>
